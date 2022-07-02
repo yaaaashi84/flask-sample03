d4ff36407581314eb98603a62dc6e4390dc21a30
yaaashi84 <yaaaashi.84@gmail.com>
2022-07-02 17:09:54 +0900

    リファクタリング

--- 01.py
+++ 01.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "GET":
-        return render_template('index.html')
-    if request.method == "POST":
-        message = request.form["message"]
-        return render_template('index.html', message=message)
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+@app.route("/", methods=["POST"])
+def index_post():
+    message = request.form["message"]
+    return render_template("index.html", message=message)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000, debug=True)
